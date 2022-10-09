fc82de1d5f7de029fbad11e7cb1770dc71f802b2
Ahmed <ahmed.nader1994@gmail.com>
2022-10-09 20:46:46 +0200

    Add network interceptors

---
+++ app/src/main/java/com/organization/nytimes/data/api/interceptors/LoggingInterceptor.kt
@@ -0,0 +1,11 @@
+package com.organization.nytimes.data.api.interceptors
+
+import com.organization.nytimes.utils.Logger
+import okhttp3.logging.HttpLoggingInterceptor
+import javax.inject.Inject
+
+class LoggingInterceptor @Inject constructor() : HttpLoggingInterceptor.Logger {
+    override fun log(message: String) {
+        Logger.i(message)
+    }
+}
